62500f3f1b09c45c035b14853199f18f3e054af1
Jocsan Mairena <jocsan.mairena@gmail.com>
2022-06-12 23:11:49 -0400

    removed .mvn wrapper

--- src/main/java/com/fordevs/mongodbspringbootdatajpa/MongodbSpringBootDataJpaApplication.java
+++ src/main/java/com/fordevs/mongodbspringbootdatajpa/MongodbSpringBootDataJpaApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.fordevs.mongodbspringbootdatajpa;
+package com.fordevs.mongodbspringbootdatajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
